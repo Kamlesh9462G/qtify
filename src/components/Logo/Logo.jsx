3206d86cc7279a7301ca49ef6153deeb3c9aec72
Kamlesh9462G <kamleshgurjar950@gmail.com>
2023-09-13 06:12:53 +0530

    built navbar section

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import logo from "../../assets/logoImage.png"
+const Logo = () => {
+  return (
+    <img src={logo} alt="logo" width={67} />
+  )
+}
+
+export default Logo
